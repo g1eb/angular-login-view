14956b7a8f200d67721a05ef12425c9dbd02c76e
Gleb <git@gleb.nl>
2016-09-23 12:07:55 +0200

    Added document binding to close login view on click outside and esc keypress.

--- src/login-view.js
+++ src/login-view.js
@@ -19,6 +19,22 @@ directive('loginView', ['$document', function ($document) {
       return {
         pre: function () {},
         post: function (scope, element) {
+          $document.on('mousedown', function (e) {
+            if ( !!scope.active && !element[0].contains(e.target) ) {
+              scope.$apply(function () {
+                scope.active = false;
+                scope.onClose();
+              });
+            }
+          });
+          $document.on('keyup', function (e) {
+            if ( !!scope.active && e.keyCode === 27 ) {
+              scope.$apply(function () {
+                scope.active = false;
+                scope.onClose();
+              });
+            }
+          });
         }
       };
     }
