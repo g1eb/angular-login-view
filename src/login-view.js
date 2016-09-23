0a58a8dc95880c29ed4a165de4a4048f74597c7c
Gleb <git@gleb.nl>
2016-09-23 10:49:37 +0200

    Let login-view be an element instead of attrubute directive for now.

--- src/login-view.js
+++ src/login-view.js
@@ -1,18 +1,19 @@
 'use strict';
 
 /**
- * Login view (general purpose login view)
+ * Login view (general purpose directive)
  */
 angular.module('g1b.login-view', []).
 directive('loginView', ['$document', function ($document) {
   return {
-    restrict: 'A',
+    restrict: 'E',
+    replace: true,
+    templateUrl: './login-view.html',
     scope: {
       user: '=',
       onLogin: '&',
       onClose: '&',
     },
-    templateUrl: './login-view.html',
     compile: function () {
       return {
         pre: function preLink() {},
